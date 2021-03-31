8d9a0ffbb8dbafd3c8ad499f13a44474a139f4d4
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-03-30 22:13:59 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import logo from "./logo.svg";
+///import logo from "./logo.svg";
 import "./App.css";
 import { Component, Fragment } from "react";
 import FormularioCadastro from "./components/FormularioCadastro";
+import { Container } from "@material-ui/core";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <Container component="article" maxWidth="sm">
         <section className="App-header">
           <h1 className="App-header_text">FORMULÁRIO DE CADASTRO</h1>
         </section>
-        <FormularioCadastro />
-      </Fragment>
+        <FormularioCadastro aoEnviar={aoEnviarForm} />
+      </Container>
     );
   }
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados);
 }
 
 export default App;
