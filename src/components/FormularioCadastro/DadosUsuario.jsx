1c7fb78d08b8337ed52cfad1361094f98ef306a8
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-04-01 22:58:49 -0300

    Add files via upload

---
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,13 @@
+import { TextField } from "@material-ui/core";
+import React from "react";
+import { TextField } from "@material-ui/core/TextField";
+
+function DadosUsuario() {
+  retur(
+    <form>
+      <TextField id="email" label="email" type="email" />
+    </form>
+  );
+}
+
+export default DadosUsuario;
--- src/components/FormularioCadastro/index.js
+++ src/components/FormularioCadastro/index.js
@@ -1,2 +1,5 @@
 import FormularioCadastro from "./FormularioCadastro";
 export default FormularioCadastro;
+
+///import DadosPessoais from "./DadosPessoais";
+///export default DadosPessoais;
