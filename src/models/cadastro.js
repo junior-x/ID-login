a3b04c8657caf80aef5f90db6d1291f8455e7b48
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-04-04 17:13:57 -0300

    Add files via upload

---
+++ src/models/cadastro.js
@@ -0,0 +1,16 @@
+function validarCPF(cpf){
+    if(cpf.length !== 11){
+      return {valido:false, texto:"CPF deve ter 11 digitos."}
+    }else{
+      return {valido:true, texto:""}
+    }
+  }
+  function validarSenha(senha){
+    if(senha.length < 4 || senha.lenght > 72){
+      return {valido:false, texto:"Senha deve ter 4 e 72 digitos."}
+    }else{
+      return {valido:true, texto:""}
+    }
+  }
+
+  export {validarCPF, validarSenha}
