3a2dc7dbb6bfb2aa5e746de980d68efda778ee8f
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-04-04 17:14:29 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component, Fragment } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 import { Container } from "@material-ui/core"
+import { validarCPF, validarSenha} from "./models/cadastro.js";
 
 class App extends Component {
   render() {
     return (
       <Container component="article" maxWidth="sm" className="App">
         <h1>Formulário de cadastro</h1>
-        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+       <FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{cpf:validarCPF, senha:validarSenha}} />
       </Container>
     );
     
@@ -18,12 +19,6 @@ function aoEnviarForm(dados){
   console.log(dados);
 }
 
-function validarCPF(cpf){
-  if(cpf.length !== 11){
-    return {valido:false, texto:"CPF deve ter 11 digitos."}
-  }else{
-    return {valido:true, texto:""}
-  }
-}
+
 
 export default App;
